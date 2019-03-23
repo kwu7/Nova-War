d7117b5dbec570e1d09bc7d7b05a9f6663658776
Yveder Joseph <josephy1@wit.edu>
2019-03-22 22:00:31 -0400

    Updated Nova-War

--- Nova-War/src/novaWarPackage/NovaWar.java
+++ Nova-War/src/novaWarPackage/NovaWar.java
@@ -1,10 +1,38 @@
 package novaWarPackage;
 
-public class NovaWar {
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class NovaWar extends BasicGame {
+
+	public NovaWar(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
