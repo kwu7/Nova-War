65deb1e6d5a02d6d2e11f578894bade9826aab16
creema <creema@WIT45001457.wit.private>
2019-03-28 15:11:11 -0400

    this isnt done but you asked  for it

---
+++ Nova-War/src/novaWarPackage/Shot.java
@@ -0,0 +1,16 @@
+package novaWarPackage;
+
+public class Shot {
+	int xStart;
+	int yStart;
+	int speed;
+	int damage;
+	
+	public Shot(int xStart, int yStart, int speed, int damage) {
+		this.xStart = xStart;
+		this.yStart = yStart;
+		this.speed = speed;
+		this.damage = damage;
+	}
+
+}
